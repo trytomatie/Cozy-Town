48b8a208fba9dbbb1c7630824bf20c7fa8ea14e2
Trytomatie <trytomatie@gmail.com>
2024-04-04 03:11:58 +0200

    Cameracontrolls added

--- Cozy Town/Assets/CameraController.cs
+++ Cozy Town/Assets/CameraController.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public float cameraSpeed = 1.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GameManager.PlayerInputMap.Camera.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+    }
+
+    private void Movement()
+    {
+        print(GameManager.PlayerInputMap.Camera.Vertical.ReadValue<float>());
+        float horizontal = GameManager.PlayerInputMap.Camera.Horizontal.ReadValue<float>();
+        float vertical = GameManager.PlayerInputMap.Camera.Vertical.ReadValue<float>();
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+        // Move depending on main camera's forward direction
+        Vector3 moveDirection = Camera.main.transform.forward * direction.z + Camera.main.transform.right * direction.x;
+        moveDirection.y = 0;
+        transform.position += moveDirection * cameraSpeed * Time.deltaTime;
     }
 }
---
+++ Cozy Town/Assets/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour
+{
+    private PlayerInput playerInputMap;
+
+
+    // Singelton
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            playerInputMap = new PlayerInput();
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public static PlayerInput PlayerInputMap
+    {         get
+        {
+            return instance.playerInputMap;
+        }
+    }
+}
