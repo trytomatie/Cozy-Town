810ced5a41d36943f398ec02cae856e16a0c2e78
trytomatie <trytomatie@gmail.com>
2024-05-23 19:55:21 +0200

    Added Screenshots, Removed Fraudgulent Gameobjects in GameSetup

--- Cozy Town/Assets/Scripts/BuildingObject.cs
+++ Cozy Town/Assets/Scripts/BuildingObject.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.Design.Serialization;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
 public class BuildingObject : MonoBehaviour
@@ -42,3 +43,42 @@ public enum BuildingType
     Building,
     Vegetation
 }
+
+#if UNITY_EDITOR
+[UnityEditor.CustomEditor(typeof(BuildingObject)), CanEditMultipleObjects]
+public class BuildingObjectEditor : UnityEditor.Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        BuildingObject buildingObject = (BuildingObject)target;
+
+        if (GUILayout.Button("Assign Sprite"))
+        {
+            if(!AssignSprite(buildingObject))
+            {
+                Debug.LogError("Sprite not found for " + buildingObject.name);
+            }
+        }
+    }
+    
+
+    private bool AssignSprite(BuildingObject target)
+    {
+        // Get sprite from Screenshot folder
+        string path = "Assets/Screenshots/" + target.name + ".png";
+        Sprite sprite = UnityEditor.AssetDatabase.LoadAssetAtPath<Sprite>(path);
+        if(sprite == null)
+        {
+            Debug.LogError("Sprite not found at path: " + path);
+            return false;
+        }
+        target.sprite = sprite;
+        // Set Dirty
+        EditorUtility.SetDirty(target);
+        // Save Prefab
+        PrefabUtility.SavePrefabAsset(target.gameObject);
+        return true;
+    }
+}
+#endif
